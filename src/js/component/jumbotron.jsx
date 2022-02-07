cf90e2fe01dc28b37693c9a8dcd4b66b19f5a75f
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-02-07 20:32:00 +0000

    Add jumbotron

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -1,3 +1,15 @@
-const Jumbotron = () => <div></div>;
+import React from "react";
+
+const Jumbotron = () => (
+	<div className="jumbotron">
+		<h1>A Warm Welcome!</h1>
+		<p className="lead">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+			condimentum sed nibh et blandit. Vivamus molestie nisi nec libero
+			gravida, et iaculis nibh hendrerit.
+		</p>
+		<button className="btn btn-primary">Call to action!</button>
+	</div>
+);
 
 export default Jumbotron;
