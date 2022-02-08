009266ad64a8ba467f55fe5c25a8bd4d4f55cc3c
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-02-08 21:04:03 +0000

    Tweak Card

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,19 +1,23 @@
-import React from "react"
+import React from "react";
 
 const Card = ({
-    imgSrc = "https://via.placeholder.com/500x325",
-    buttonText = "Find out more!",
-    title,
-    body,
+	imgSrc = "https://via.placeholder.com/500x325",
+	buttonText = "Find out more!",
+	title,
+	body,
 }) => (
-    <div>
-        <img className="card-img-top" src={imgSrc} />
-        <div class="card-body">
-            <h5 class="card-title"> { title } </h5>
-            <p class="card-text"> { body } </p>
-            <a href="#" class="btn btn-primary"> { buttonText } </a>
-        </div>
-    </div>
-)
+	<div className="card text-center">
+		<img className="card-img-top" src={imgSrc} />
+		<div class="card-body">
+			<h5 class="card-title"> {title} </h5>
+			<p class="card-text"> {body} </p>
+		</div>
+		<div className="card-footer">
+			<a href="#" class="btn btn-primary">
+				{buttonText}
+			</a>
+		</div>
+	</div>
+);
 
 export default Card;
