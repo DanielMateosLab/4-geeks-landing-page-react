56dcbd37978ad25b257b9061806f6d44f280efb5
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-02-08 21:04:29 +0000

    Add content and styles to cardList

--- src/js/component/cardList.jsx
+++ src/js/component/cardList.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import Card from "./card.jsx";
 
 const CardList = () => {
-	return <section className="card-list"></section>;
+	const cards = [
+		{
+			title: "Aenean eleifend",
+			body: "Suspendisse massa augue, tempus non dictum vel, pulvinar at arcu. Phasellus maximus faucibus justo ac imperdiet. Sed vitae arcu fringilla, varius tortor non, dictum nisl.",
+		},
+		{
+			title: "Volutpat tellus",
+			body: "Donec molestie eleifend leo eget viverra. Mauris efficitur mattis est, vitae imperdiet nisl consequat ut.  Ut sed ultricies ante, quis tincidunt mauris.",
+		},
+		{
+			title: "Pellentesque condimentum",
+			body: "Sed eu suscipit mi. Nam mollis velit eu magna tincidunt feugiat. Proin eleifend commodo magna et varius. Ut eget ex id ipsum sodales tempor ut in dolor.",
+		},
+		{
+			title: "Donec molestie",
+			body: "Maecenas tristique neque nisl, nec lacinia enim faucibus in. Pellentesque lobortis, quam ac consequat auctor, turpis arcu commodo odio, at consectetur felis lectus non ante.",
+		},
+	];
+	return (
+		<section className="card-list">
+			{cards.map((card) => (
+				<Card {...card} />
+			))}
+		</section>
+	);
 };
 
 export default CardList;
